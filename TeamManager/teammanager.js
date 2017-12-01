6daee305a228384cb98befbd59f2a9e24fb20d06
Mark Kazanski <markkazanski@gmail.com>
2017-11-30 19:16:19 -0600

    team manager

---
+++ TeamManager/teammanager.js
@@ -0,0 +1,34 @@
+function Players(name, position, offence, defence){
+    this.name = name;
+    this.position = position;
+    this.offence = offence;
+    this.defence = defence;
+
+    this.goodGame = function(){
+        if( randomIntFromInterval(0,1) ){
+            this.offence += 10;
+        }else  
+            this.defence += 10;
+    };
+
+    this.badGame = function(){
+        if( randomIntFromInterval(0,1) )
+            this.offence -= 10;
+        else  
+            this.defence -= 10;
+    };
+
+    this.printStats = function(){
+        for(var x in this){
+            if(typeof this[x] === "number" || typeof this[x] === "string")
+                console.log(x + ": " + this[x]);
+        }
+        console.log(" ");
+    }; 
+
+}
+
+function randomIntFromInterval(min,max){
+    return Math.floor(Math.random()*(max-min+1)+min);
+}
+
