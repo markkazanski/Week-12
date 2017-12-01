1ad1c68a44da84a78c03ecc65b555dd5fc4304fd
Mark Kazanski <markkazanski@gmail.com>
2017-11-30 19:10:04 -0600

    players

---
+++ players.js
@@ -0,0 +1,11 @@
+function Players(name, position, offence, defence){
+    this.name = name;
+    this.position = position;
+    this.offence = offence;
+    this.defence = defence;
+
+    this.goodGame = function(){
+        
+    };
+
+}
